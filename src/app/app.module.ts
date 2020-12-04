f4a0c78726f5ed15e67449802b31185b6e5edae4
Gabe-T <gabriel.tarrant@hotmail.com>
2020-12-04 17:35:56 -0500

    text and sidenav started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { MatSidenavModule} from '@angular/material/sidenav';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { SynthComponent } from './synth/synth.component';
+
+
 
 
 @NgModule({
@@ -21,7 +25,9 @@ import { SynthComponent } from './synth/synth.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    MatSidenavModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
