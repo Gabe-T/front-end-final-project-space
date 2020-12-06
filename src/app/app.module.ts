5b84620914338033365a0fc7ed6e1229fa17716b
sard1 <josephsardashti@gmail.com>
2020-12-06 14:16:23 -0500

    added expansion panel, removed sidenav. actually did not test tone :'(

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { MatSidenavModule} from '@angular/material/sidenav';
+import { MatExpansionModule} from '@angular/material/expansion'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -27,6 +28,7 @@ import { SynthComponent } from './synth/synth.component';
     HttpClientModule,
     FormsModule,
     MatSidenavModule,
+    MatExpansionModule,
     BrowserAnimationsModule
   ],
   providers: [],
