b56a40409e15be192089dd9732366d31c7478586
sard1 <josephsardashti@gmail.com>
2020-12-08 00:16:40 -0500

    added first functionality to synth component, audio working

--- src/app/synth/synth.component.ts
+++ src/app/synth/synth.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Player } from 'tone';
+//import { Tone } from 'tone/build/esm/core/Tone';
+import * as Tone from 'tone';
 @Component({
   selector: 'app-synth',
   templateUrl: './synth.component.html',
@@ -7,7 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SynthComponent implements OnInit {
 
-  constructor() { }
+  synth1:any;
+  synth2:any;
+  msdown:boolean = false;
+  mixer:any;
+  delay:any;
+  reverb:any;
+  constructor() {
+    this.synth1 = new Tone.Synth();
+    this.synth2 = new Tone.Synth();
+    this.mixer = new Tone.Gain();
+    this.delay = new Tone.PingPongDelay({
+      wet: .75,
+      delayTime: "4n",
+      feedback: .5
+    });
+    this.reverb = new Tone.Reverb({
+      wet: .75,
+      decay: 20
+    })
+    //mix buss
+    this.synth1.connect(this.mixer);
+    this.synth2.connect(this.mixer);
+    this.mixer.connect(this.delay);
+    this.delay.connect(this.reverb);
+    this.reverb.toDestination();
+    
+   }
+
+   play(){
+    this.synth1.triggerAttackRelease("C3", "8n");
+    this.synth2.triggerAttackRelease("E4", "8n")
+  }
 
   ngOnInit(): void {
   }
