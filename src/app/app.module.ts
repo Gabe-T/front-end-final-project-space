89926acb20898ee1a6aa95f4994423a95df31c7d
sard1 <josephsardashti@gmail.com>
2020-12-08 12:00:09 -0500

    added slider components and connected to synth parameters

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { MatSidenavModule} from '@angular/material/sidenav';
 import { MatExpansionModule} from '@angular/material/expansion'
+import {MatSliderModule} from '@angular/material/slider';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -29,6 +29,7 @@ import { SynthComponent } from './synth/synth.component';
     FormsModule,
     MatSidenavModule,
     MatExpansionModule,
+    MatSliderModule,
     BrowserAnimationsModule
   ],
   providers: [],
--- src/app/synth/synth.component.ts
+++ src/app/synth/synth.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Player } from 'tone';
+import { OmniOscillator, Player } from 'tone';
 //import { Tone } from 'tone/build/esm/core/Tone';
 import * as Tone from 'tone';
 @Component({
@@ -15,10 +15,13 @@ export class SynthComponent implements OnInit {
   mixer:any;
   delay:any;
   reverb:any;
+  slider1Value:number = 100;
+  slider2Value:number = 200;
+  slider3Value:number = 0.01
   constructor() {
     this.synth1 = new Tone.Synth();
     this.synth2 = new Tone.Synth();
-    this.mixer = new Tone.Gain();
+    this.mixer = new Tone.Gain(.5);
     this.delay = new Tone.PingPongDelay({
       wet: .75,
       delayTime: "4n",
@@ -26,7 +29,7 @@ export class SynthComponent implements OnInit {
     });
     this.reverb = new Tone.Reverb({
       wet: .75,
-      decay: 20
+      decay: this.slider1Value,
     })
     //mix buss
     this.synth1.connect(this.mixer);
@@ -36,10 +39,14 @@ export class SynthComponent implements OnInit {
     this.reverb.toDestination();
     
    }
+   getSliderValue(event:any){
+    this.reverb.decay = this.slider3Value;
+   }
 
    play(){
-    this.synth1.triggerAttackRelease("C3", "8n");
-    this.synth2.triggerAttackRelease("E4", "8n")
+    this.synth1.triggerAttackRelease(this.slider1Value, "8n");
+    this.synth2.triggerAttackRelease(this.slider2Value, "8n")
+  
   }
 
   ngOnInit(): void {
